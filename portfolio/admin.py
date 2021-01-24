f3dcb2820ec04dd497c0fc43327e5ede014b5077
Zee98 <zamanehsani@gmail.com>
2021-01-24 16:30:35 +0400

    some changes in admin page

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import Post, Index
 
+admin.site.site_header = "Zaman's Portfolio "
+admin.site.site_title = "Zaman"
+admin.site.index_title = "Admin"
 
 admin.site.register(Post)
 admin.site.register(Index)
